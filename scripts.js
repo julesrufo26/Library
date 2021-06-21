487c59b2dfe0f27a6f73d989e214a7ca15449134
Jules Rufo <julesreegorufo@gmail.com>
2021-06-21 19:40:23 +0800

    Add firebase realtime database

--- scripts.js
+++ scripts.js
@@ -11,77 +11,19 @@ function Book(title, author, numberOfPages, hasRead){
     this.hasRead = hasRead;
 }
 
-function addBookToLibrary(book){
-    myLibrary.push(book);
+function clearFormInput(modal){
+    let inputs = modal.querySelectorAll("input");
+    let readNo = document.getElementById("readNo");
+    modal.style.display = "none";
 
-    //clear local Storage then save new contents of the array
-    localStorage.clear();
-    localStorage.setItem('libraryArray', JSON.stringify(myLibrary));
-}   
-
-function closeForm(){
-    const inputs = document.querySelectorAll('#form-modal > input');
-    const readYes = document.getElementById('readYes');
-    const readNo = document.getElementById('readNo');
-
-    //clear textBox contents
-    inputs.forEach(input =>{
-        input.value = "";
+    //clear all input values except for radio buttons
+    inputs.forEach(input => {
+        if(input.type != "radio"){
+            input.value = "";
+        }
     });
 
-    //Bring back the values for radio buttons
-    readYes.value = 'Yes';
-    readNo.value = "No";
-
-    //reset radio button to No
     readNo.checked = true;
-
-    //hide the modal
-    modal.style.display = 'none';
-}
-
-function displayData(){
-    const table = document.querySelector('#main > table');
-    let x = 0;
-
-    myLibrary.forEach(book => {
-        let tr = document.createElement('tr');
-        let tdTitle = document.createElement('td');
-        let tdAuthor = document.createElement('td');
-        let tdNumPages = document.createElement('td');
-        let tdHasRead = document.createElement('td');
-        let tdDelete = document.createElement('td');
-        
-        tdTitle.textContent = book.title;
-        tdAuthor.textContent = book.author;
-        tdNumPages.textContent = book.numberOfPages;
-
-        //for hasRead, create a link then append it to tdHasRead
-        let aHasRead = document.createElement('a');
-        aHasRead.textContent = book.hasRead;
-        aHasRead.setAttribute('data-attribute', x); //added index number for update purposes
-        aHasRead.style.cssText = "cursor: pointer; text-decoration: underline; color: grey;";
-        addReadListener(aHasRead);
-        tdHasRead.appendChild(aHasRead);
-
-        //for delete, also create a link then append it to tdDelete
-        let aDelete = document.createElement('a');
-        aDelete.textContent = 'Delete';
-        aDelete.setAttribute('data-attribute', x); //added index number for delete purposes
-        aDelete.style.cssText = "cursor: pointer; text-decoration: underline; color: red;";
-        addDelListener(aDelete);
-        tdDelete.appendChild(aDelete);
-
-        tr.appendChild(tdTitle);
-        tr.appendChild(tdAuthor);
-        tr.appendChild(tdNumPages);
-        tr.appendChild(tdHasRead);
-        tr.appendChild(tdDelete);
-
-        table.appendChild(tr);
-
-        x++;
-    });
 }
 
 function clearData(){
@@ -95,105 +37,135 @@ function clearData(){
     })
 }
 
-function addDelListener(element){
-   element.addEventListener('click', (e)=>{
-       let index = e.toElement.dataset.attribute;
-       deleteElement(index);
-   });
-}
-
-function deleteElement(index){
-    myLibrary.splice(index, 1);
-
-    //update array in the localStorage
-    localStorageUpdate();
-
-    //update display
-    clearData();
-    displayData();
-}
-
-function addReadListener(element){
-    element.addEventListener('click', (e)=>{
-        let index = e.toElement.dataset.attribute;
-        toggleRead(index);
-    });
-}
-
-function toggleRead(index){
-    let currentReadStatus = myLibrary[index].hasRead;
-
-    if(currentReadStatus == 'Yes'){
-        myLibrary[index].hasRead = 'No';
-    }
-    else{
-        myLibrary[index].hasRead = 'Yes';
-    }
-
-    //update array in the localStorage
-    localStorageUpdate();
-
-    //update display
-    clearData();
-    displayData();
-}
-
-function localStorageUpdate(){
-    localStorage.clear();
-    localStorage.setItem('libraryArray', JSON.stringify(myLibrary));
-    myLibrary = JSON.parse(localStorage.getItem('libraryArray'));
-}
-
-window.addEventListener('load', () => {
-    const addNewBook = document.getElementById('add');
-    const modal = document.getElementById('modal');
-    const cancel = document.getElementById('cancel');
-    const submit = document.getElementById('submit');
-    const title = document.getElementById('title');
-    const author = document.getElementById('author');
-    const pages = document.getElementById('pages');
-    const read = document.getElementsByName('read');
-
-    //Check if localStorage is not empty
-    if(localStorage.length != 0){
-        //if true, load the array to myLibrary array
-        myLibrary = JSON.parse(localStorage.getItem('libraryArray'));
-        displayData();
-    }
-
-    addNewBook.addEventListener('click', () =>{
-        modal.style.display = 'flex';
-    });
-
-    cancel.addEventListener('click', () =>{
-        closeForm();
-    });
-
-    submit.addEventListener('click', () => {
-        let hasRead = "";
-        //simple form validation to check if all fields have been filled up
-        if(title.value == "" || author.value == "" || pages.value == ""){
-            alert("All fields must be filled up.");
-            return;
-        }
-
-        for(let x=0; x<read.length; x++){
-            if(read[x].checked){
-                hasRead = read[x].value;
-
-                break;
-            }
-        }
-
-        //Create new book object
-        let book = new Book(title.value, author.value, pages.value, hasRead);    
-
-        //Add to the myLibrary array
-        addBookToLibrary(book);
-
-        closeForm();
+function displayData(){
+    const table = document.querySelector('#main > table');
+    const dbRefObject = firebase.database().ref();
+    dbRefObject.on('value', snap => {
 
         clearData();
-        displayData();
+
+        snap.forEach(book => {
+            let tr = document.createElement('tr');
+            let tdTitle = document.createElement('td');
+            let tdAuthor = document.createElement('td');
+            let tdNumPages = document.createElement('td');
+            let tdHasRead = document.createElement('td');
+            let tdDelete = document.createElement('td');
+
+            tdTitle.textContent = book.val().title;
+            tdAuthor.textContent = book.val().author;
+            tdNumPages.textContent = book.val().numberOfPages;
+
+            //for hasRead, create a link then append it to tdHasRead
+            let aHasRead = document.createElement('a');
+            aHasRead.textContent = book.val().alreadyRead;
+            aHasRead.setAttribute('data-attribute', book.key); //added index number for update purposes
+            aHasRead.style.cssText = "cursor: pointer; text-decoration: underline; color: grey;";
+            addReadListener(aHasRead, aHasRead.textContent);
+            tdHasRead.appendChild(aHasRead);
+
+            //for delete, also create a link then append it to tdDelete
+            let aDelete = document.createElement('a');
+            aDelete.className = "del";
+            aDelete.textContent = 'Delete';
+            aDelete.setAttribute('data-attribute', book.key); //added index number for delete purposes
+            aDelete.style.cssText = "cursor: pointer; text-decoration: underline; color: red;";
+            addDelListener(aDelete);
+            tdDelete.appendChild(aDelete);
+
+            tr.appendChild(tdTitle);
+            tr.appendChild(tdAuthor);
+            tr.appendChild(tdNumPages);
+            tr.appendChild(tdHasRead);
+            tr.appendChild(tdDelete);
+
+            table.appendChild(tr);
+        });
+    });
+}
+
+function insertData(){
+    const dbRefObject = firebase.database().ref();
+    const title = document.getElementById("title");
+    const author = document.getElementById("author");
+    const pages = document.getElementById("pages");
+    const alreadyRead = document.getElementsByName("read");
+    let read = "";
+    
+    alreadyRead.forEach(x => {
+        if(x.checked){
+            read = x.value;
+        }
+    });
+
+    clearData();
+
+    dbRefObject.push({
+        title: title.value,
+        author: author.value,
+        numberOfPages: pages.value,
+        alreadyRead: read
+    });
+}
+
+function addDelListener(btn){
+    btn.addEventListener("click", () => {
+        deleteEntry(btn.dataset.attribute);
+    });
+}
+
+function deleteEntry(key){
+    const dbRefObject = firebase.database().ref().child(key);
+    dbRefObject.set(null);
+}
+
+function addReadListener(btn, value){
+    btn.addEventListener("click", () => {
+        toggleRead(btn.dataset.attribute, value);
+    });
+}
+
+function toggleRead(key, value){
+    const dbRefObject = firebase.database().ref().child(key);
+    let newValue = (value == "No" ? "Yes" : "No");
+
+    dbRefObject.update({
+        alreadyRead: newValue
+    });
+}
+
+window.addEventListener("load", () => {
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    var firebaseConfig = {
+        apiKey: "AIzaSyDNf80WN1o1-PsJlgyCwO9mgR9WzYa--bs",
+        authDomain: "library-e163c.firebaseapp.com",
+        projectId: "library-e163c",
+        storageBucket: "library-e163c.appspot.com",
+        messagingSenderId: "859801603285",
+        appId: "1:859801603285:web:e031d0048dd0897ddf665c",
+        measurementId: "G-VNWN11G3BL"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    let addBtn = document.getElementById("add");
+    let modal = document.getElementById("modal");
+    let cancel = document.getElementById("cancel");
+    let submit = document.getElementById("submit");
+    
+    displayData();
+
+    addBtn.addEventListener("click", () => {
+        modal.style.display = "flex";
+    });
+
+    cancel.addEventListener("click", () => {
+        clearFormInput(modal);
+    });
+
+    submit.addEventListener("click", () => {
+        insertData();
+        clearFormInput(modal);
     });
 });
